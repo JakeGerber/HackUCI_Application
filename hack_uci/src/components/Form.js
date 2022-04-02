a0907d77cb6949e8eb601e82e453d0030d99e48b
Jake Gerber <jake.j.gerber@gmail.com>
2022-04-02 14:51:44 -0700

    More code to inputs and button

--- hack_uci/src/components/Form.js
+++ hack_uci/src/components/Form.js
@@ -1,13 +1,38 @@
 import './Form.css'
+import { useState } from "react";
 
 function Form() {
 
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [funfact, setFunfact] = useState("")
 
     return (
         <div id="main-container">
             <form>
                 Wowza
-                <input type="text" name="name" autocomplete="off" className='input-field'/>
+                <input 
+                    type="text" 
+                    autocomplete="off" 
+                    placeholder="Name" 
+                    className='input-field'
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input 
+                    type="text" 
+                    autocomplete="off" 
+                    placeholder="Email" 
+                    className='input-field'
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input 
+                    type="text" 
+                    autocomplete="off" 
+                    placeholder="Fun Fact" 
+                    className='input-field'
+                    onChange={(e) => setFunfact(e.target.value)}
+                />
+                <button type="button">Submit</button>
             </form>
         </div>
     )
