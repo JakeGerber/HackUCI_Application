5c2943a5d99726a7e11724ee0cb2c2404f073284
Jake Gerber <jake.j.gerber@gmail.com>
2022-04-02 14:25:14 -0700

    Added Input

---
+++ hack_uci/src/Home.js
@@ -0,0 +1,14 @@
+import './Home.css'
+import Form from "./components/Form.js"
+
+function Home() {
+
+    return (
+    <div id = "gradient">
+        Hello!
+        <Form />
+    </div>
+    )
+}
+
+export default Home
---
+++ hack_uci/src/components/Form.js
@@ -0,0 +1,16 @@
+import './Form.css'
+
+function Form() {
+
+
+    return (
+        <div id="main-container">
+            <form>
+                Wowza
+                <input type="text" name="name" autocomplete="off" className='input-field'/>
+            </form>
+        </div>
+    )
+}
+
+export default Form
